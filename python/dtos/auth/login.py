36c3cb0043e9a833a5d068f8a9d727809a7a90c4
Jaxx1911 <truonghoangnguyen1911@gmail.com>
2024-09-29 18:04:57 +0700

    add: base reponse

--- python/dtos/auth/login.py
+++ python/dtos/auth/login.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
 
+from python.models.base.reponse import BaseResponse
+
+
 @dataclass
-class LoginDTO:
+class LoginRequest:
     username: str
     password: str
+
+
+class LoginResponse(BaseResponse):
+    x: str
