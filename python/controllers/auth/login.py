d02d8c13aa707c6293124e82fd96dc65fc589ff1
Luong Tuan Anh <dulieu.vblc@gmail.com>
2024-10-01 21:29:43 +0700

    add: api login

--- python/controllers/auth/login.py
+++ python/controllers/auth/login.py
@@ -1,7 +1,15 @@
+from dataclasses import dataclass
+
 from common.di.manager import inject
 from dtos.auth.login import LoginDTO
+from models.response.template import Unauthorized, Success
 from services.auth.login import LoginService
 from utils.singleton import singleton
+
+
+@dataclass
+class LoginData:
+    access_token: str
 
 
 @singleton
@@ -12,8 +20,9 @@ class LoginController:
 
     def loginWithCredentials(self, body: LoginDTO):
         data = self.service.login(body.username, body.password)
-        return {
-            "token": data
-        }
+        if data is None:
+            return Unauthorized(message="Username or password was wrong!")
+        return Success(message="Login successfully", data=LoginData(access_token=data))
+
 
 loginController = LoginController()
---
+++ python/models/response/template.py
@@ -0,0 +1,69 @@
+from typing import Generic, TypeVar, Literal
+from starlette import status
+from starlette.responses import JSONResponse
+from pydantic import BaseModel
+
+T = TypeVar('T')
+
+
+class BaseResponse(BaseModel):
+    status: Literal['failed', 'success']
+
+
+class SuccessResponse(Generic[T], BaseResponse):
+    message: str
+    data: T
+    status: Literal['success'] = 'success'
+
+
+class BadRequestResponse(BaseResponse):
+    message: str
+    code: str = '0'
+    status: Literal['failed'] = 'failed'
+
+
+class FailedResponse(BaseResponse):
+    message: str
+    status: Literal['failed'] = 'failed'
+
+
+# Hàm trả về JSONResponse
+def Success[T](message: str, data: T) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=SuccessResponse(
+            message=message,
+            data=data
+        ).dict()  # Chuyển đổi đối tượng thành dict
+    )
+
+
+def Created[T](message: str, data: T) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content=SuccessResponse(
+            message=message,
+            data=data
+        ).dict()
+    )
+
+
+def Unauthorized(message: str) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        content=FailedResponse(message=message).dict()
+    )
+
+
+def BadRequest(message: str, code: str) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=BadRequestResponse(message=message, code=code).dict()
+    )
+
+
+def Failed(message: str) -> JSONResponse:
+    return JSONResponse(
+        content=FailedResponse(message=message).dict(),
+        status_code=status.HTTP_200_OK
+    )
